a4180816c21ebd7dbcb26f46e7ef708e0f00a091
Andrew <anelson5@1800contacts.com>
2020-05-26 11:22:25 -0600

    init

---
+++ MyFirstApi/ApiController/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MyFirstApi.ApiController
+{
+    [RoutePrefix("api/Values")]
+    public class ValuesController: System.Web.Http.ApiController
+    {
+        private static List<string> _values = new List<string>
+        {
+            "one",
+            "two",
+            "three"
+        };
+
+        [HttpGet]
+        [Route("")]
+        public List<string> GetValues()
+        {
+            return _values;
+        }
+
+        [HttpGet]
+        [Route("{identifier}")]
+        public string GetValue(int identifier)
+        {
+            return _values[identifier];
+        }
+
+        [HttpPost]
+        [Route("")]
+        public int PostValue(ValuesRequestBody request)
+        {
+            _values.Add(request.Value);
+            return _values.Count - 1;
+        }
+
+        [HttpPut]
+        [Route("{identifier")]
+        public int PutValue([FromUri]int identifier, [FromBody]ValuesRequestBody request)
+        {
+            _values[identifier] = request.Value;
+            return identifier;
+        }
+
+        [HttpDelete]
+        [Route("{identifier")]
+        public void DeleteValue(int identifier)
+        {
+            _values.RemoveAt(identifier);
+        }
+    }
+
+    public class ValuesRequestBody
+    {
+        public string Value { get; set; }
+    }
+}
---
+++ MyFirstApi/Configuration/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Http;
+
+namespace MyFirstApi.Configuration
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Attribute routing.
+            config.MapHttpAttributeRoutes();
+
+            // Convention-based routing.
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "GetValues",
+            //    routeTemplate: "api/{controller}/{identifier}",
+            //    defaults: new { controller="Values", identifier = RouteParameter.Optional }
+            //);
+        }
+    }
+}
